a3858f51c6b298a6204ee8f702fc47b5378cd9b4
Upender Devulapally <upender.devulapally@gmail.com>
2016-06-23 20:28:12 +0530

    Login and Register templates are dialogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  #the form calls helpers which are specified by Devise and so aren't present when you access a Devise form from a non-Devise controller.
+  #To get around this, need to add the following methods to the helper class of the controller you wish to display the form under.
+  # Alternatively, we can just add them to application helper to make them available anywhere.
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
