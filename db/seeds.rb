402f54f5a8097280604f7639556516c33d26e03b
Upender Devulapally <upender.devulapally@gmail.com>
2016-06-23 15:50:18 +0530

    seeds as per new model changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,75 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Preference.destroy_all
+
+{'Tour'=> 'TR','Camera'=> 'CAM', 'Location'=> 'LN'}.each do |key,val|
+  Preference.create(name: key, abbreviation: val)
+end
+
 Platform.destroy_all
 ['iOS', 'Android'].each do |name|
   Platform.create(name: name)
 end
+# Platform Categories
+platform = Platform.where(name: 'iOS').first
+platform.platform_categories.create(name: 'iPad')
+platform.platform_categories.create(name: 'iPhone')
+
 
 
 #User.destroy_all
-User.where(email: 'testuser@test.com').destroy_all
+User.where(email: 'testuser@test.com').first.destroy!
 user = User.create(fname: 'test', lname: 'user', email: 'testuser@test.com',password: '12345678', password_confirmation: '12345678')
 
 
 # Create Application
-App.destroy_all
-application = user.apps.create(name: 'test iOS',platform_ids: [ Platform.all.map(&:id) ], description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat.')
+user.apps.each do |app|
+  app.destroy!
+end
+#application = user.apps.create(name: 'test iOS',platform_ids: [ Platform.all.map(&:id) ], description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat.')
 
-Tour.destroy_all
-# Tour Setup
-tour = application.create_tour(name: 'Test Tour')
-
-# Assets
+app = user.apps.new(platforms: [Platform.first],name: 'test iOS',
+                    description: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat.',
+                    sw_version: '1.0.0')
+# Create Tour
+tour_preference = Preference.tour
 ['1','2','3'].each do |n|
   File.open(Rails.root.join('app', 'assets', 'images', 'home', "background#{n}.jpg")) do |f|
-    tour.guides.create(source: f, heading: 'Header text', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu velit bibendum, hendrerit arcu nec, dictum est. Praesent lobortis tincidunt.')
+    pre_prompt = app.pre_prompts.build(source: f, preference: tour_preference,
+                                       header: 'HeaderContent..', footer: 'Footer Content', content: 'Some Content')
+    ['1','2'].each do |m|
+      File.open(Rails.root.join('app', 'assets', 'images', 'button_actions', "#{m}.png")) do |f|
+        pre_prompt.button_actions.build(source: f, btn_text: m)
+      end
+    end
   end
 end
+
+app.save!
+
+# Create other preferences(Cam, Location etc)
+
+Preference.nin(name: 'Tour').each do |preference|
+  File.open(Rails.root.join('app', 'assets', 'images', 'home', "background1.jpg")) do |f|
+    pre_prompt = app.pre_prompts.build(source: f, preference: preference,
+                                       header: 'HeaderContent..', footer: 'Footer Content', content: 'Some Content')
+    ['1','2'].each do |m|
+      File.open(Rails.root.join('app', 'assets', 'images', 'button_actions', "#{m}.png")) do |f|
+        pre_prompt.button_actions.build(source: f, btn_text: m)
+      end
+    end
+  end
+end
+
+app.save!
+
+
+
+
+
+
+
+
 
 
