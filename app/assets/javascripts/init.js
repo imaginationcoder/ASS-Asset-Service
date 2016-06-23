c057f074a6ca18aa3da929fe102855a6a206db19
Upender Devulapally <upender.devulapally@gmail.com>
2016-06-23 19:31:19 +0530

    new bootstrap template as home page

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -2,11 +2,11 @@ $( document ).ready(function() {
     // $(".dropdown-button").dropdown();
     //Enable trubolinks prgress indicator
     Turbolinks.enableProgressBar();
-    $('.button-collapse').sideNav();
+    /*$('.button-collapse').sideNav();
     $('.parallax').parallax();
     $(".dropdown-button").dropdown();
     $(".device-dropdown-button").dropdown();
 
     //$('#modal1').openModal();
-    $('.modal-trigger').leanModal();
+    $('.modal-trigger').leanModal();*/
 });
