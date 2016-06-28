2da203e10b468fd4ccbbc4a15346fdbe42542fe5
Upender Devulapally <upender.devulapally@gmail.com>
2016-06-28 12:11:21 +0530

    button actions based on type
    unset other attributes based on btn type so they don't persist in DB

--- app/models/button_action.rb
+++ app/models/button_action.rb
@@ -1,5 +1,7 @@
 class ButtonAction
   include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Paranoia
 
 
   field :text
@@ -9,5 +11,18 @@ class ButtonAction
   mount_uploader :source, AssetUploader, dependent: :destroy
 
   embedded_in :pre_prompt
+  before_save :check_btn_type
+
+  def check_btn_type
+    # remove other unused elements before saving to db based on btn_type
+    case self.btn_type
+      when 0
+        self.unset(:text, :text_position, :source)
+      when 1
+        self.unset(:source)
+      when 2
+        self.unset(:text, :text_position)
+    end
+  end
 
 end
