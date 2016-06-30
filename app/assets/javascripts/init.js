32bfa9b19dce79f7418cfa82c5ed5591871d315c
Upender Devulapally <upender.devulapally@gmail.com>
2016-06-30 17:30:36 +0530

    tour assets gallery view

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -24,4 +24,12 @@ $( document ).ready(function() {
         type:'image'  
     });
 
+    $('.magnific-popup-gallery').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
 });
