10a35715b24a9e639f8a7950cbae9d149ba29ac9
Upender Devulapally <upender.devulapally@gmail.com>
2016-07-01 16:00:51 +0530

    flash messages in Login page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,4 +44,9 @@ module ApplicationHelper
     end
   end
 
+  def bootstrap_class_for flash_type
+    { success: 'alert-success', error: 'alert-danger',
+      alert: 'alert-warning', notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
