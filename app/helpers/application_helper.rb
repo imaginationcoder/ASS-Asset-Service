e8d237d9b8b9e0ce66508345d934243959077d62
Upender Devulapally <upender.devulapally@gmail.com>
2016-07-01 13:11:16 +0530

    elegant(dynamic) page title and meta keywords and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,33 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  # elegant page title
+  def page_title(title = nil)
+    if title.present?
+      content_for :title, title
+    else
+      content_for?(:title) ? t('app.title') + ' | ' + content_for(:title) : t('app.title')
+    end
+  end
+
+  # SEO meta tags based on page
+  # in view <%= meta_keywords('hello', 'world') %>
+  def meta_keywords(tags = nil)
+    if tags.present?
+      content_for :meta_keywords, tags
+    else
+      kws = t('app.meta.keywords')
+      content_for?(:meta_keywords) ? [content_for(:meta_keywords), kws].join(', ') : kws
+    end
+  end
+  # SEO meta description based on page
+  # in view <%= meta_description('Hello page based meta description goes here') %>
+  def meta_description(desc = nil)
+    if desc.present?
+      content_for :meta_description, desc
+    else
+      content_for?(:meta_description) ? content_for(:meta_description) : t('app.meta.description')
+    end
+  end
+
 end
