1ef962d459b5dba67790412f6d665e45d204f2e9
Upender Devulapally <upender.devulapally@gmail.com>
2016-07-12 21:17:41 +0530

    bug fix and progressBar color change

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -31,19 +31,7 @@ $( document ).ready(function() {
     $('[data-toggle="tooltip"]').tooltip({
         trigger : 'hover'
     });
-
-    // Home page login, register popups
-    $('.popup-with-zoom-anim').magnificPopup({
-        type: 'inline',
-        fixedContentPos: false,
-        fixedBgPos: true,
-        overflowY: 'auto',
-        closeBtnInside: true,
-        preloader: false,
-        midClick: true,
-        removalDelay: 300,
-        mainClass: 'my-mfp-zoom-in'
-    });
+ 
 
     $('.magnific-popup-img').magnificPopup({
         type:'image'
