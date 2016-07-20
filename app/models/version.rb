74e1be4b96a9bbf3b754a61d8df0b05673aed9f8
Upender Devulapally <upender.devulapally@gmail.com>
2016-07-20 20:33:23 +0530

    Model: handling version stauts change event

--- app/models/version.rb
+++ app/models/version.rb
@@ -19,6 +19,28 @@ class Version
 
   # after publish update published=> true and editing=> false and create another version with count+1(unless same version exists)
   # and create all templates with new version(editing one)
+  before_update :check_if_published
+
+
+  private
+  def check_if_published
+    # self.published_changed?(from: false, to: true)
+    if self.published_changed? and self.published.eql?(true)
+      app_versions = self.app.versions
+      # set published time to now
+      self.published_at = Time.now
+      self.editing = false
+      # except this update all to unpublished and editing false
+      app_versions.where(:id.ne => self.id).update(published: false, editing: false)
+      count = app_versions.count + 1
+      # then create a new version(i.e editing one) unless editing version exists
+      unless app_versions.where(number: count).exists?
+        app_versions.create(number: count)
+        #TODO create app templates with newly generated version
+      end
+    end
+  end
+
 
 
 end
