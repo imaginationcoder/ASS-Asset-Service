474ba51ac7db2f463262ac4a4369513ef07bb827
Upender Devulapally <upender.devulapally@gmail.com>
2016-07-22 12:13:40 +0530

    if app is published create new version and templates for it

--- Gemfile
+++ Gemfile
@@ -87,3 +87,6 @@ gem 'bootstrap-social-rails'
 gem "font-awesome-rails"
 gem "font-ionicons-rails"
 gem 'active_link_to'
+# Simple gem for copying CarrierWave files between resources.
+# It's solving issues with local and remote storage discussed here
+gem 'copy_carrierwave_file'
--- app/models/version.rb
+++ app/models/version.rb
@@ -15,29 +15,41 @@ class Version
   ## Associations  ---------------------------------------- ###
   embedded_in :app
 
-
-
-  # after publish update published=> true and editing=> false and create another version with count+1(unless same version exists)
-  # and create all templates with new version(editing one)
-  before_update :check_if_published
+  before_update :if_published_create_version_and_templates
 
 
   private
-  def check_if_published
-    # self.published_changed?(from: false, to: true)
+  # before publishing version, update editing and published attrs for other versions
+  # and create a new version if doesn't exists and create templates for newly generated version
+  def if_published_create_version_and_templates
     if published_changed? and published.eql?(true)
       # except this update all to unpublished and editing false
       app.versions.update_all(published: false, published_at: nil)
       # set published time to now
-      self.published_at = Time.now
-      self.editing = false
-      self.published = true
+      self.published_at = Time.now;
+      self.editing = false; self.published = true
       count = app.versions.count + 1
       # then create a new version(i.e editing one) unless editing version exists
       unless app.versions.where(editing: true).exists?
         app.versions.update(editing: false)
         app.versions.create(number: count, editing: true)
-        #TODO create app templates with newly generated version
+        # Now create app templates with newly generated version by cloning previous version's templates
+        app.templates.each do |template|
+          cloned_template = template.clone
+          cloned_template.app_version = count # update app_version with newly generated one
+          # Set timestamps to nil so that new timestamps are added when saved
+          cloned_template.created_at = nil;cloned_template.updated_at = nil
+          # copy source of original template to cloned template
+          CopyCarrierwaveFile::CopyFileService.new(template, cloned_template, :source).set_file
+          cloned_template.text_assets.each  {|ts| ts.created_at = nil;ts.updated_at = nil }
+          cloned_template.button_actions.each_with_index do |bc, index|
+            bc.created_at = nil; bc.updated_at = nil
+            if template.button_actions[index].btn_type.eql?(2)
+              CopyCarrierwaveFile::CopyFileService.new(template.button_actions[index], bc, :source).set_file
+            end
+          end
+          cloned_template.save
+        end
       end
     end
   end
