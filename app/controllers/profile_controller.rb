ab26286809defef0cb969182f4c2a084c7365a42
Upender Devulapally <upender.devulapally@gmail.com>
2016-07-25 17:02:01 +0530

    user password update

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,7 +5,7 @@ class ProfileController < ApplicationController
   def index; end
 
   def update_avatar
-    if current_user.update_attributes(avatar_params)
+    if current_user.update(avatar_params)
      flash[:success] = 'Avatar changed successfully.'
      redirect_to profile_url
     else
@@ -15,15 +15,29 @@ class ProfileController < ApplicationController
 
   # update profile
   def update
-    if current_user.update_attributes(profile_params)
+    if current_user.update(profile_params)
       flash[:success] = 'Profile updated successfully.'
+      redirect_to profile_url
+    else
+      render 'index'
+    end
+  end
+
+  # update password
+  def update_password
+    @user = current_user
+    #To use "confirm_password" field to force user to enter old password before updating with the new one:
+    # Change @user.update(user_params) to @user.update_with_password(user_params)
+    if @user.update_with_password(password_params)
+      # Sign in the user by passing validation in case their password changed
+      sign_in @user, bypass: true
+      flash[:success] = 'Password updated successfully.'
       redirect_to profile_url
     else
       render 'index'
     end
 
   end
-
 
   protected
   def avatar_params
@@ -32,6 +46,10 @@ class ProfileController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:fname,:lname,:email,:phone,:company,:designation,:notes)
+  end
+
+  def password_params
+    params.require(:user).permit(:current_password,:password,:password_confirmation)
   end
 
 end
