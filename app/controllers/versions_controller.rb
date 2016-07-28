7ac7b9cb0f87c41bacc4ca3e2c99a0f6c736ef5d
Upender Devulapally <upender.devulapally@gmail.com>
2016-07-28 16:46:58 +0530

    UI app version preview

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -2,7 +2,7 @@ class VersionsController < ApplicationController
   layout 'adminlte'
   before_action :authenticate_user!
   before_action :load_app
-
+  before_action :load_version, except: :index
 
   def index
     @versions = @app.versions.order_by(number: :desc)
@@ -10,16 +10,24 @@ class VersionsController < ApplicationController
   end
 
   def publish
-    @version = @app.versions.find(params[:version_id])
     @version.update(published: true)
     #flash[:notice] = "Version #{@version.number} published successfully."
     render nothing: true
   end
 
+  def preview
+    templates = @app.templates.where(app_version: @version.number)
+    @tour_templates = templates.where(permission_id: Permission.tour.id)
+    @permission_templates =  templates.where(:permission_id.ne=> Permission.tour.id)
+  end
 
   private
   def load_app
     @app = App.find(params[:id])
+  end
+
+  def load_version
+    @version = @app.versions.find(params[:version_id])
   end
 
 end
