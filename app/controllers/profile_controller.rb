2132ca9b8fa1539df5ab10ab800bf08d93be5283
Upender Devulapally <upender.devulapally@gmail.com>
2016-08-09 18:49:25 +0530

    socailLogin: finish_signup step if email not provided

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,7 @@
 class ProfileController < ApplicationController
   layout 'adminlte'
-  before_action :authenticate_user!
+  layout 'authentication' , only: [:finish_signup,:sent_email_instructions]
+  before_action :authenticate_user!, except: [:sent_email_instructions]
 
   def index; end
 
@@ -36,6 +37,18 @@ class ProfileController < ApplicationController
 
   end
 
+  def finish_signup
+    if request.patch? && params[:user] #&& params[:user][:email]
+      if current_user.update(finish_signup_params)
+        #current_user.skip_reconfirmation!
+        #sign_in(current_user, :bypass => true)
+        redirect_to sent_email_instructions_path, notice: t('devise.confirmations.send_instructions')
+      else
+        @show_errors = true
+      end
+    end
+  end
+
   protected
   def avatar_params
     params.require(:user).permit(:avatar)
@@ -47,6 +60,12 @@ class ProfileController < ApplicationController
 
   def password_params
     params.require(:user).permit(:current_password,:password,:password_confirmation)
+  end
+
+  def finish_signup_params
+    accessible = [ :full_name, :email ] # extend with your own params
+    accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
+    params.require(:user).permit(accessible)
   end
 
 end
