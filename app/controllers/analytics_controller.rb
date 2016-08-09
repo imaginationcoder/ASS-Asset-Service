376ecd06e0e412671940f6e80505af5afd0e3408
Upender Devulapally <upender.devulapally@gmail.com>
2016-08-09 10:53:26 +0530

    analytics: exception handling for non published version

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -4,7 +4,7 @@ class AnalyticsController < ApplicationController
   before_action :load_app
 
   def index
-    @version = params[:version] || @app.published_version.number || @app.editing_version.number
+    @version = params[:version] || @app.published_version.try(:number) || @app.editing_version.number
     @templates = @app.templates.where(app_version: @version)
     tour_permission = Permission.tour
     @tour_templates = @templates.where(permission_id: tour_permission.id)
