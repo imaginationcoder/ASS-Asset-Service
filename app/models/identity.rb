ac216a61f465c76d5953a8e43caed39a4592c022
Upender Devulapally <upender.devulapally@gmail.com>
2016-08-09 18:53:30 +0530

    socialLogin: model indexing

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -10,6 +10,9 @@ class Identity
   validates_presence_of :uid, :provider
   validates_uniqueness_of :uid, scope: :provider
 
+  ## Indexes
+  index({ provider: 1, uid: 1 })
+
   def self.find_for_oauth(auth)
     find_or_create_by(uid: auth.uid, provider: auth.provider)
   end
