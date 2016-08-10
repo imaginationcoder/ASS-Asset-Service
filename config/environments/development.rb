b971075bd4d3fd0991567d7badd4ea1c99dd348d
Upender Devulapally <upender.devulapally@gmail.com>
2016-08-10 16:50:56 +0530

    SMTP(gmail) setup for prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,19 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
-  
+  # SMTP GMAIL Settings
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.smtp_settings = {
+  #     address: "smtp.gmail.com",
+  #     port: 587,
+  #     domain: "localhost:3000",
+  #     authentication: "plain",
+  #     enable_starttls_auto: true,
+  #     user_name: ENV["GMAIL_USERNAME"],
+  #     password: ENV["GMAIL_PASSWORD"]
+  # }
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,22 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #SMTP Settings
+  config.action_mailer.default_url_options = { :host => 'ass-assets.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "ass-assets.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+  }
+
+
 end
