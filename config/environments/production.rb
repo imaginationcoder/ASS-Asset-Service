be9f8fcce518066e21d2b38bd2c31466e6cad8ed
Upender Devulapally <upender.devulapally@gmail.com>
2016-08-10 17:26:53 +0530

    smtp domain name change for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "ass-assets.herokuapp.com",
+      domain: "gmail.com",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: ENV["GMAIL_USERNAME"],
